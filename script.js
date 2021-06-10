f85145b0a4b901cf29e114b0f75042d6566d6275
HafsaWoyessa <84517071+Hafsa-Woyessa@users.noreply.github.com>
2021-06-10 16:29:49 +0000

    line 2

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let age = 12
 let guess = 0
 
-while (guess != age ) {
- guess = prompt('Guess my age')
- if (guess == age) {
-  alert('You win!')
-} else if (guess < age) {
-   alert('You guessed too small.Please try again')
-} else if  (guess > age) {
-  alert('You guessed too big.Please try again')
+while (guess !== age) {
+  guess = prompt('Guess my age')
+  if (guess === age) {
+    alert('You win!')
+  } else if (guess < age) {
+     alert('You guessed too small.Please try again')
+} else if (guess > age) {
+   alert('You guessed too big.Please try again')
 }
 }
